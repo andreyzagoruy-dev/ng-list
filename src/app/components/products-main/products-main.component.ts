9728450605e3b072aabbe011f1f4f633e034eeab
andreyzagoruy-dev <andreyzagoruy.dev@gmail.com>
2020-01-29 17:07:08 +0200

    Initial commit

---
+++ src/app/components/products-main/products-main.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import products from '../../constants/products';
+
+@Component({
+  selector: 'app-products-main',
+  templateUrl: './products-main.component.html',
+  styleUrls: ['./products-main.component.scss']
+})
+export class ProductsMainComponent implements OnInit {
+
+  public products = products;
+
+  public filteredProducts = products;
+
+  public sortDirection = -1;
+
+  ngOnInit() {
+    this.toggleSort('Rating');
+  }
+
+  toggleSort(field) {
+    this.filteredProducts = this.filteredProducts.sort((a,b) => (a[field] > b[field]) ? 1 * this.sortDirection : ((b[field] > a[field]) ? -1 * this.sortDirection : 0));
+    this.sortDirection *= -1;
+  }
+
+  search(inputEl) {
+    const value = inputEl.value;
+    this.filteredProducts = this.products.filter(product => product.Title.toLowerCase().match(value.toLowerCase()));
+  }
+
+}
